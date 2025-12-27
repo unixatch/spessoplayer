7da920bb5d63d5d29aaa4ab95a9a7e3d5ba7a6d6
unixatch <76928074+unixatch@users.noreply.github.com>
2025-12-27 19:25:53 +0100

    Adding utils.mjs, simplifying some regexes, ...
    adding loopStart, loopEnd, fixing some checks not working properly
    and lastly updating the help text

--- cli.mjs
+++ cli.mjs
@@ -16,6 +16,7 @@
 */
 
 import { join } from "path"
+import "./utils.mjs"
 
 const actUpOnPassedArgs = async (args) => {
   let lastParam;
@@ -23,15 +24,19 @@ const actUpOnPassedArgs = async (args) => {
   if (newArguments.length !== 0) {
     for (const arg of newArguments) {
       switch (arg) {
-        case /^(?:.*\.mid)$/.test(arg) && arg: {
+        case /^.*\.mid$/.test(arg) && arg: {
           global.midiFile = arg;
           break;
         }
-        case /^(?:.*\.sf2)$/.test(arg) && arg: {
+        case /^.*\.sf2$/.test(arg) && arg: {
           global.soundfontFile = arg;
           break;
         }
-        case /^(?:-)$/.test(arg) && arg: {
+        case /^.*(?:\.wav|\.wave)$/.test(arg) && arg: {
+          global.waveFile = arg;
+          break;
+        }
+        case /^-$/.test(arg) && arg: {
           global.toStdout = true;
           break;
         }
@@ -41,6 +46,22 @@ const actUpOnPassedArgs = async (args) => {
           if (newArguments[indexOfArg + 1] === undefined) throw new ReferenceError("Missing necessary argument");
           
           lastParam = "sample-rate"
+          break;
+        }
+        case /^(?:--loop-start|\/loop-start|-ls|\/ls)$/.test(arg) && arg: {
+          // In case there's no other argument
+          const indexOfArg = newArguments.indexOf(arg);
+          if (newArguments[indexOfArg + 1] === undefined) throw new ReferenceError("Missing necessary argument");
+          
+          lastParam = "loop-start"
+          break;
+        }
+        case /^(?:--loop-end|\/loop-end|-le|\/le)$/.test(arg) && arg: {
+          // In case there's no other argument
+          const indexOfArg = newArguments.indexOf(arg);
+          if (newArguments[indexOfArg + 1] === undefined) throw new ReferenceError("Missing necessary argument");
+          
+          lastParam = "loop-end"
           break;
         }
         case /^(?:--loop|\/loop|-l|\/l)$/.test(arg) && arg: {
@@ -65,6 +86,14 @@ const actUpOnPassedArgs = async (args) => {
             setLoop(arg)
             break;
           }
+          if (lastParam === "loop-start") {
+            setLoopStart(arg)
+            break;
+          }
+          if (lastParam === "loop-end") {
+            setLoopEnd(arg)
+            break;
+          }
           if (lastParam === "sample-rate") {
             setSampleRate(arg)
             break;
@@ -79,28 +108,82 @@ const actUpOnPassedArgs = async (args) => {
           process.exit()
       }
     }
+    if (global?.waveFile === undefined) global.waveFile = "out.wav"
   }
 }
 
 const setLoop = arg => {
-  if (!/^(?:Infinity|infinity)$/.test(arg)
-      || typeof Number(arg) !== "number") {
-    throw new TypeError("Passed something that wasn't either Infinity or any number")
+  if (typeof Number(arg) === "number"
+      && !/^(?:Infinity|infinity)$/.test(arg)) {
+    global.parameter = "--loop";
+    global.loopN = Number(arg);
+    return;
   }
-  global.parameter = "--loop";
-  global.loopN = arg;
+  if (/^(?:Infinity|infinity)$/.test(arg)) {
+    throw new Error("Can't use infinity, sorry")
+  }
+  throw new TypeError("Passed something that wasn't a number")
+}
+const setLoopStart = arg => {
+  if (typeof Number(arg) === "number"
+      || Date.parse(`1970T${arg}Z`) !== NaN) {
+    if (/[0-9]{1,2}:[0-9]{2}:[0-9]{2}(\.[0-9])*/.test(arg)) {
+      const seconds = Date.parse(`1970T${arg}Z`) / 1000;
+      global.parameter = "--loop-start";
+      global.loopStart = seconds;
+      return;
+    }
+    global.parameter = "--loop-start";
+    global.loopStart = Number(arg);
+    return;
+  }
+  throw new TypeError("Passed something that wasn't a number or in ISO string format")
+}
+const setLoopEnd = arg => {
+  if (typeof Number(arg) === "number"
+      || Date.parse(`1970T${arg}Z`) !== NaN) {
+    if (/[0-9]{1,2}:[0-9]{2}:[0-9]{2}(\.[0-9])*/.test(arg)) {
+      const seconds = Date.parse(`1970T${arg}Z`) / 1000;
+      global.parameter = "--loop-end";
+      global.loopStart = seconds;
+      return;
+    }
+    global.parameter = "--loop-end";
+    global.loopStart = Number(arg);
+    return;
+  }
+  throw new TypeError("Passed something that wasn't a number or in ISO string format")
 }
 const setSampleRate = arg => {
-  global.parameter = "--sample-rate";
-  global.sampleRate = arg;
+  if (typeof Number(arg) === "number") {
+    global.parameter = "--sample-rate";
+    global.sampleRate = Number(arg);
+    return;
+  }
+  throw new TypeError("Passed something that wasn't a number")
 }
 const help = () => {
   const helpText = `${underline}spessoplayer${normal}
   ${dimGrayBold}A midi converter that uses spessasynth_core to generate the data${normal}
   
+  Usage:
+    ${bold}spessoplayer${normal} [${dimGray}options${normal}] <midi> <soundfont>
+  
   Available parameters:
     ${green}--loop${normal}, ${green}/loop${normal}, ${green}-l${normal}, ${green}/l${normal}:
       ${dimGray+italics}Loop x amount of times${normal}
+      ${dimGray+italics}(It might be slow with bigger numbers)${normal}
+      
+    ${green}--loop-start${normal}, ${green}/loop-start${normal}, ${green}-ls${normal}, ${green}/ls${normal}:
+      ${dimGray+italics}When the loop starts${normal}
+      
+    ${green}--loop-end${normal}, ${green}/loop-end${normal}, ${green}-le${normal}, ${green}/le${normal}:
+      ${dimGray+italics}When the loop ends${normal}
+      
+    ${green}--sample-rate${normal}, ${green}/sample-rate${normal}, ${green}-r${normal}, ${green}/r${normal}:
+      ${dimGray+italics}Sample rate to use${normal}
+      ${dimGray+italics}(It might be slow with bigger numbers for players like mpv)${normal}
+      ${dimGray+italics}(Some players might downsize it to a smaller frequency)${normal}
       
     ${green}--help${normal}, ${green}/help${normal}, ${green}-h${normal}, ${green}/h${normal}, ${green}/?${normal}:
       ${dimGray+italics}Shows this help message${normal}
