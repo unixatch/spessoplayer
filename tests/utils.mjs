11fd54437bd667efc02e4f0eabe7244dbc8e0672
unixatch <76928074+unixatch@users.noreply.github.com>
2026-04-04 21:30:06 +0200

    Use the proper argument for toFile

--- tests/utils.mjs
+++ tests/utils.mjs
@@ -37,7 +37,7 @@ function addOptionalArgumentsToStdout(args) {
   return args;
 }
 function addOptionalArgumentsToFile(args) {
-  if (process.argv.includes("-f"))   args.push("-f", "flac")
+  if (process.argv.includes("-fo"))  args.push("out.flac")
   if (process.argv.includes("-rvb")) args.push("-rvb", "20")
   if (process.argv.includes("-l"))   args.push("-l", "1")
   return args;
