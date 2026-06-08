dc93cec6759efe586ace23997cc40bf99944c45f
unixatch <76928074+unixatch@users.noreply.github.com>
2026-06-08 06:21:56 +0200

    Improved powershell completion by adding...
    ..short versions of parameters,
    added the default path completion,
    moved on to using foreach..in instead and
    less importantly, ' → "

--- powershell_completion.ps1
+++ powershell_completion.ps1
@@ -14,129 +14,130 @@
 #   along with spessoplayer.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-[scriptblock]$ScriptBlock = {
-    param( $WordToComplete )
+[scriptblock] $ScriptBlock = {
+    param($wordToComplete)
 
     $Options = @(
         @{
-            CompletionText = '--input'
-            ToolTip = 'Takes the following file and puts it in the list by n'
+            CompletionText = "--input", "-i"
+            ToolTip = "Takes the following file and puts it in the list by n"
         },
         @{
-            CompletionText = '--volume'
-            ToolTip = 'Volume to set (default: 100%) (formats: dB, percentages, decimals)'
+            CompletionText = "--volume", "-vol"
+            ToolTip = "Volume to set (default: 100%) (formats: dB, percentages, decimals)"
         },
         @{
-            CompletionText = '--reverb-volume'
-            ToolTip = 'Volume to set for reverb (default: none)'
+            CompletionText = "--reverb-volume", "-rvb"
+            ToolTip = "Volume to set for reverb (default: none)"
         },
         @{
-            CompletionText = '--loop'
-            ToolTip = 'Loop x amount of times (default: 0)'
+            CompletionText = "--loop", "-l"
+            ToolTip = "Loop x amount of times (default: 0)"
         },
         @{
-            CompletionText = '--loop-start'
-            ToolTip = 'The loop will start after seconds'
+            CompletionText = "--loop-start", "-ls"
+            ToolTip = "The loop will start after seconds"
         },
         @{
-            CompletionText = '--loop-end'
-            ToolTip = 'The loop will restart at [-]seconds from the end'
+            CompletionText = "--loop-end", "-le"
+            ToolTip = "The loop will restart at [-]seconds from the end"
         },
         @{
-            CompletionText = '--loop-fade'
-            ToolTip = 'It does 1 more loop on top of yours and then it fades away slowly based on loop-fade-start (Doesn''''t work without the loop parameter turned on)'
+            CompletionText = "--loop-fade", "-lF"
+            ToolTip = "It does 1 more loop on top of yours and then it fades away slowly based on loop-fade-start (Doesn""""t work without the loop parameter turned on)"
         },
         @{
-            CompletionText = '--loop-fade-start'
-            ToolTip = 'When the loop fade starts (default: 1)'
+            CompletionText = "--loop-fade-start", "-lFs"
+            ToolTip = "When the loop fade starts (default: 1)"
         },
         @{
-            CompletionText = '--loop-fade-duration'
-            ToolTip = 'How much the loop fade should last (default: 4)'
+            CompletionText = "--loop-fade-duration", "-lFd"
+            ToolTip = "How much the loop fade should last (default: 4)"
         },
         @{
-            CompletionText = '--sample-rate'
-            ToolTip = 'Sample rate to use (default: 48000)'
+            CompletionText = "--sample-rate", "-r"
+            ToolTip = "Sample rate to use (default: 48000)"
         },
         @{
-            CompletionText = '--format'
-            ToolTip = 'Format to use for stdout (default: wav) (formats: wav, mp3, flac, pcm, f32le)'
+            CompletionText = "--format", "-f"
+            ToolTip = "Format to use for stdout (default: wav) (formats: wav, mp3, flac, pcm, f32le)"
         },
         @{
-            CompletionText = '--threads'
-            ToolTip = 'Sets the amount of threads to use when writing to files.'
+            CompletionText = "--threads", "-T"
+            ToolTip = "Sets the amount of threads to use when writing to files."
         },
         @{
-            CompletionText = '--show-usage'
-            ToolTip = 'Shows RAM usage and CPU time. (Only works in file mode)'
+            CompletionText = "--show-usage", "-U"
+            ToolTip = "Shows RAM usage and CPU time. (Only works in file mode)"
         },
         @{
-            CompletionText = '--text-delay'
-            ToolTip = 'Changes how fast it renders text (default: 500) (Only works in file mode)'
+            CompletionText = "--text-delay", "-d"
+            ToolTip = "Changes how fast it renders text (default: 500) (Only works in file mode)"
         },
         @{
-            CompletionText = '--no-progress'
-            ToolTip = 'Disables progress text rendering (Only works in file mode)'
+            CompletionText = "--no-progress", "-np"
+            ToolTip = "Disables progress text rendering (Only works in file mode)"
         },
         @{
-            CompletionText = '--confirmation'
-            ToolTip = 'Asks for confirmation before proceeding'
+            CompletionText = "--confirmation", "-c"
+            ToolTip = "Asks for confirmation before proceeding"
         },
         @{
-            CompletionText = '--no-table'
+            CompletionText = "--no-table", "-nt"
             ToolTip = "When asking for confirmation, it'll show the information in a JSON-like format instead of a table"
         },
         @{
-            CompletionText = '--dry-run'
+            CompletionText = "--dry-run", "-dr"
             ToolTip = "Runs the program as normal but it'll write to /dev/null on unix and \\.\nul on windows."
         },
         @{
-            CompletionText = '--verbose'
-            ToolTip = 'Sets the verbosity (default: 2)'
+            CompletionText = "--verbose", "-v"
+            ToolTip = "Sets the verbosity (default: 2)"
         },
         @{
-            CompletionText = '--enable-spessasynth-logging'
+            CompletionText = "--enable-spessasynth-logging"
             ToolTip = "Enables spessasynth's logging system, basically only used for debugging."
         },
         @{
-            CompletionText = '--enable-spessasynth-warn-logging'
+            CompletionText = "--enable-spessasynth-warn-logging"
             ToolTip = "Enables spessasynth's logging system, basically only used for debugging."
         },
         @{
-            CompletionText = '--enable-spessasynth-info-logging'
+            CompletionText = "--enable-spessasynth-info-logging"
             ToolTip = "Enables spessasynth's logging system, basically only used for debugging."
         },
         @{
-            CompletionText = '--log-file'
-            ToolTip = 'Sets path to the log file (default: ./spesso.log)'
+            CompletionText = "--log-file", "-lf"
+            ToolTip = "Sets path to the log file (default: ./spesso.log)"
         },
         @{
-            CompletionText = '--uninstall'
-            ToolTip = 'Uninstalls dependencies with confirmation and the entire program'
+            CompletionText = "--uninstall", "-u"
+            ToolTip = "Uninstalls dependencies with confirmation and the entire program"
         },
         @{
-            CompletionText = '--help'
-            ToolTip = 'Shows the help message'
+            CompletionText = "--help", "-h"
+            ToolTip = "Shows the help message"
         },
         @{
-            CompletionText = '--version'
-            ToolTip = 'Show the installed version'
-        }
+            CompletionText = "--version", "-V"
+            ToolTip = "Show the installed version"
+        },
+        @{ CompletionText = " " }
     )
 
-    $Options.Where({$_.CompletionText -like "$wordToComplete*"}) | `
-        ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new(
-                $_.CompletionText,
-                $_.CompletionText,
-                'ParameterValue',
-                $_.ToolTip
-            )
-        }
+    foreach ($object in $Options) {
+        $matchingText = $object.CompletionText -like "$wordToComplete*"
+        if (!$matchingText) { continue }
+
+        [System.Management.Automation.CompletionResult]::new(
+            $matchingText, $matchingText,
+            "ParameterValue",
+            $object.ToolTip
+        )
+    }
 }
 
 Register-ArgumentCompleter `
-    -CommandName spessoplayer `
-    -Native `
+    -CommandName spessoplayer -Native `
     -ScriptBlock $ScriptBlock
 
